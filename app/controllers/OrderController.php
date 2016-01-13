49da485237f03b29de0eded09082f15dfbb048be
Yousaf <yousafml786@gmail.com>
2016-01-14 01:31:23 +0500

    Commit

--- app/controllers/OrderController.php
+++ app/controllers/OrderController.php
@@ -10,6 +10,14 @@ class OrderController extends BaseController {
     public function index() {
         $orders = Order::all();
         return View::make('order.index');
+        // get the POST data
+        $input_data = Input::all();
+        $order = new Order();
+        if ($order->validate($input_data)){
+            //save data to order
+        }else {
+            $errors = $b->errors();
+        }
     }
 
 }
